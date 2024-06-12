dc3fcba8bfb06cdc02c550955f1f5ea1d0d505c0
Omar Khan <omar.khan2@ucalgary.ca>
2024-06-11 22:51:48 -0600

    no repeat jumps, fixed side jump

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -31,6 +31,7 @@ public class JumpController : MonoBehaviour
     public float jumpForce = 2.0f;
 
     public bool isGrounded;
+    public bool readyToGo;
     public Rigidbody rb;
 
     private float leftVel = 1;
@@ -50,25 +51,37 @@ public class JumpController : MonoBehaviour
         yRotation = mainCamera.transform.eulerAngles.y;
         JumpDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
 
-        if (upJB.bounds.Intersects(leftHand.bounds) && upJB.bounds.Intersects(rightHand.bounds) && isGrounded)
+        if (isGrounded &&
+            !upJB.bounds.Intersects(leftHand.bounds) &&
+            !upJB.bounds.Intersects(rightHand.bounds) &&
+            !forwardJB.bounds.Intersects(leftHand.bounds) &&
+            !forwardJB.bounds.Intersects(rightHand.bounds) &&
+            !rightJB.bounds.Intersects(rightHand.bounds) &&
+            !leftJB.bounds.Intersects(leftHand.bounds)
+           )
+        {
+            readyToGo = true;
+        }
+
+        if (upJB.bounds.Intersects(leftHand.bounds) && upJB.bounds.Intersects(rightHand.bounds) && readyToGo)
         {
             jumpDir = new Vector3(0, 3.0f, 0);
             Jump();
         }
-        else if (forwardJB.bounds.Intersects(leftHand.bounds) && forwardJB.bounds.Intersects(rightHand.bounds) && isGrounded)
+        else if (forwardJB.bounds.Intersects(leftHand.bounds) && forwardJB.bounds.Intersects(rightHand.bounds) && readyToGo)
         {
             jumpDir = new Vector3(JumpDirection.transform.forward.x, 3.0f, JumpDirection.transform.forward.z) / 1.1f;
             Jump();
         }
-        else if (rightJB.bounds.Intersects(rightHand.bounds) && isGrounded)
+        else if (rightJB.bounds.Intersects(rightHand.bounds) && readyToGo)
         {
             jumpDir = new Vector3(JumpDirection.transform.right.x, 3.0f, JumpDirection.transform.right.z);
-            Jump();
+            Jump("right");
         }
-        else if (leftJB.bounds.Intersects(leftHand.bounds) && isGrounded)
+        else if (leftJB.bounds.Intersects(leftHand.bounds) && readyToGo)
         {
             jumpDir = new Vector3(-JumpDirection.transform.right.x, 3.0f, -JumpDirection.transform.right.z);
-            Jump();
+            Jump("left");
         }
 
     }
@@ -78,17 +91,23 @@ public class JumpController : MonoBehaviour
         if (collision.gameObject.name.Equals("Ground")) isGrounded = true;
     }
 
-    void Jump(){
+    void Jump(string side = null){
         if (_inputData._leftController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 leftVelocity))
             leftVel = leftVelocity.magnitude;
 
         if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 rightVelocity))
             rightVel = rightVelocity.magnitude;
 
-        jumpForce = Mathf.Clamp(((leftVel + rightVel) / 4), 1.0f, 2.5f);
+        float realVel;
+        if (side == null) realVel = leftVel + rightVel;
+        else if (side == "right") realVel = rightVel;
+        else realVel = leftVel;
+
+        jumpForce = Mathf.Clamp((realVel / 4.0f), 1.0f, 2.5f);
         rb.AddForce(jumpDir * jumpForce, ForceMode.Impulse);
         
         isGrounded = false;
+        readyToGo = false;
     }
     
     
